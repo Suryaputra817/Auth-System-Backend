0f2c0758a98cf0894decfefb2658a3f86a413c84
Manjit Kumar Shyanti <manjitkumarshyanti@gmail.com>
2026-05-16 11:36:34 +0530

    Fix import paths for database and config in server and email service files

--- src/app.js
+++ src/app.js
@@ -1,6 +1,6 @@
 import express from "express";
 import morgan from "morgan";
-import authRouter from "../Routes/auth.routes.js";
+import authRouter from "./Routes/auth.routes.js";
 import cookieParser from "cookie-parser";
 
 const app = express();
@@ -8,6 +8,5 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(morgan("dev"));
 app.use("/api/auth", authRouter);
-
 
 export default app;
--- src/services/email.service.js
+++ src/services/email.service.js
@@ -1,5 +1,5 @@
 import nodemailer from "nodemailer";
-import config from "../src/config/config.js";
+import config from "../config/config.js";
 
 const transporter = nodemailer.createTransport({
   service: "gmail",
